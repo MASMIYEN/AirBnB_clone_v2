e1ad5901f02befb9d3d72028e42c4983b7519ffe
0ussamabernou <oussama.bernou@outlook.com>
2023-12-19 23:14:28 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,6 +15,7 @@ class DBStorage:
         from os import getenv
         # from sqlalchemy.orm import sessionmaker # not used
 
+        hb_env = getenv("HBNB_ENV")
         user = getenv("HBNB_MYSQL_USER")
         password = getenv("HBNB_MYSQL_PWD")
         host = getenv("HBNB_MYSQL_HOST")
@@ -25,7 +26,8 @@ class DBStorage:
                                                  password, host, database),
             pool_pre_ping=True,
         )
-        if getenv("HBNB_ENV") == "test":
+        # if getenv("HBNB_ENV") == "test":
+        if hb_env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
@@ -65,17 +67,14 @@ class DBStorage:
     def reload(self):
         """Create all tables in the database and create the current database
         session"""
-        from models.base_model import Base, BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
+        from models.base_model import Base
         from sqlalchemy.orm import sessionmaker
-        from sqlalchemy import create_engine
         from sqlalchemy.orm import scoped_session
 
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)()
+        # Session = scoped_session(
+        #     sessionmaker(bind=self.__engine, expire_on_commit=False)
+        # )
+        # self.__session = Session()
