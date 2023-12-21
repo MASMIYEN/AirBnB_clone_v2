fab35ad926cb3772784328adcd738c0ae836b489
0ussamabernou <oussama.bernou@outlook.com>
2023-12-21 20:42:27 +0100

    update

--- models/user.py
+++ models/user.py
@@ -15,8 +15,8 @@ class User(BaseModel, Base):
     if storage_type == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="users", cascade="all, delete")
         # reviews = relationship("Review", cascade="all, delete")
     else:
